92838606eea7c033bd0ddc36e0b3b8e97a69df90
dadadaguai <1183984923@qq.com>
2023-03-09 18:23:04 +0800

    构建项目架构，初始化功能

---
+++ chatgpt-ai-interfaces/src/main/java/cn/ddg/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package cn.ddg.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: chatGpt-test
+ * @description: 启动入口
+ * @author: Ljd
+ * @create: 2023~03~09 18:11
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class);
+    }
+}
---
+++ chatgpt-ai-interfaces/src/test/java/cn/ddg/api/ApiTest.java
@@ -0,0 +1,11 @@
+package cn.ddg.api;
+
+/**
+ * @program: chatGpt-test
+ * @description:
+ * @author: Ljd
+ * @create: 2023~03~09 18:16
+ */
+public class ApiTest {
+
+}
